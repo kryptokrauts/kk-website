623d8c07e4c76b6aa603a458545f27aff490a391
Marco Walz <walz.marcoo@gmail.com>
2019-03-04 23:22:10 +0100

    fix prefixPath

--- src/components/centered_logo.js
+++ src/components/centered_logo.js
@@ -1,7 +1,6 @@
 import React from "react"
-import { withPrefix } from 'gatsby'
 import centeredLogoStyles from "./centered_logo.module.css"
 
 export default () => (
-    <img src={withPrefix('/img/logo_400x400.png')} className={centeredLogoStyles.container} alt="logo" />
+    <img src="/img/logo_400x400.png" className={centeredLogoStyles.container} alt="logo" />
 )

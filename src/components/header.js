97f4647fd70965626b6c314ddd8d233a69dea69c
Marco Walz <walz.marcoo@gmail.com>
2019-04-29 00:11:33 +0200

    add pizza image as example

--- src/components/header.js
+++ src/components/header.js
@@ -77,6 +77,7 @@ export default class Header extends Component {
               node {
                 prefix
                 label
+                path
               }
             }
           }

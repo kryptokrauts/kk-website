5b1a913822e55b79b85161f4322747c22b8258df
Marco Walz <walz.marcoo@gmail.com>
2019-06-13 21:38:01 +0200

    added plugins to support mermaid-diagramms and embedded twitter feeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,22 +16,37 @@ module.exports = {
         path: `./src/data/`,
       },
     },
-    `gatsby-transformer-remark`,
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-transformer-remark`,
       options: {
-        trackingIds: [
-          "UA-139263777-1", // Google Analytics
-        ],
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        pluginConfig: {
-          head: false,
-          respectDNT: true,
-        },
-      },
+        plugins: [
+          {
+            resolve: 'gatsby-remark-graph',
+            options: {
+              // this is the language in your code-block that triggers mermaid parsing
+              language: 'mermaid', // default
+              theme: 'default' // could also be dark, forest, or neutral
+            }
+          },
+          {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+              trackingIds: [
+                "UA-139263777-1", // Google Analytics
+              ],
+              gtagConfig: {
+                anonymize_ip: true,
+                cookie_expires: 0,
+              },
+              pluginConfig: {
+                head: false,
+                respectDNT: true,
+              },
+            }
+          },
+        ]
+      }
     },
+    `gatsby-plugin-twitter`
   ],
 }

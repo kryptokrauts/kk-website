63f6ec62dda25566da209c9f3611d1b5c6be8016
Marco Walz <walz.marcoo@gmail.com>
2020-02-01 12:01:23 +0100

    tmp state of timeline redesign

--- src/components/timelineItem.js
+++ src/components/timelineItem.js
@@ -2,6 +2,19 @@ import React from "react";
 import { dateFormatter, openURL } from "../utils/common.util";
 import timelineItem from "./timelineItem.module.css";
 
+let currentYear;
+
+function RenderYear(props) {
+  const year = props.date.substring(0,4);
+  if(!currentYear || currentYear > year) {
+    currentYear = year;
+    return(
+  <div className={timelineItem.year}><a href={"#" + year} className="anchor">{year}</a></div>
+    );
+  }
+  return null;
+}
+
 const renderFirst = (itemClasses) => (
   <div className={itemClasses}>
     <div className={timelineItem.icon}><i className="fas fa-calendar-day"></i></div>
@@ -11,21 +24,24 @@ const renderFirst = (itemClasses) => (
 )
 
 const renderItem = (itemData, itemClasses) => (
-  <div id={itemData.title.replace(/[. ,:-]+/g, "-").replace(/["]+/g, "")} className={itemClasses}
-    onClick={() => openURL((itemData.path ? itemData.path : itemData.externalLink), itemData.path ? false : true)}>
-    {itemData.thumbnail ? (
-      <div className={timelineItem.thumbnailContainer}>
-        <img className={timelineItem.thumbnail} src={itemData.thumbnail} alt="" />
-        <div className={timelineItem.thumbnailSource}>Image: {itemData.thumbnailSource}</div>
+  <div className={itemClasses}>
+    <RenderYear date={itemData.date}></RenderYear>
+    <div id={itemData.title.replace(/[. ,:-]+/g, "-").replace(/["]+/g, "")}
+      onClick={() => openURL((itemData.path ? itemData.path : itemData.externalLink), itemData.path ? false : true)}>
+      {itemData.thumbnail ? (
+        <div className={timelineItem.thumbnailContainer}>
+          <img className={timelineItem.thumbnail} src={itemData.thumbnail} alt="" />
+          <div className={timelineItem.thumbnailSource}>Image: {itemData.thumbnailSource}</div>
+        </div>
+      ) : ""}
+      <div className={timelineItem.icon}><i className={itemData.icon || "fas fa-bell"}></i></div>
+      <div className={timelineItem.date}>{dateFormatter(itemData.date)}</div>
+      <a href={"#" + itemData.title.replace(/[. ,:-]+/g, "-").replace(/["]+/g, "")}></a>
+      <div className={timelineItem.title}>
+        {itemData.title}
       </div>
-    ) : ""}
-    <div className={timelineItem.icon}><i className={itemData.icon || "fas fa-bell"}></i></div>
-    <div className={timelineItem.date}>{dateFormatter(itemData.date)}</div>
-    <a href={"#" + itemData.title.replace(/[. ,:-]+/g, "-").replace(/["]+/g, "")}></a>
-    <div className={timelineItem.title}>
-      {itemData.title}
+      <div className={timelineItem.line}></div>
     </div>
-    <div className={timelineItem.line}></div>
   </div>
 )
 

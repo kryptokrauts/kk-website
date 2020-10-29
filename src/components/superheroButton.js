646d0aee72d763f06b76203c6d925648b8f367a7
Marco Walz <walz.marcoo@gmail.com>
2020-10-29 17:49:27 +0100

    cdn 0.5.1

--- src/components/superheroButton.js
+++ src/components/superheroButton.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Helmet from "react-helmet";
+import React from 'react';
+import Helmet from 'react-helmet';
 
 export default () => {
     const handleLoad = () => {
-        window.superheroButton('.superhero-icon', { size: 'icon', account: 'kryptokrauts.chain' });
-        window.superheroButton('.superhero-small', { size: 'small', account: 'kryptokrauts.chain' });
-        window.superheroButton('.superhero-medium', { size: 'medium', account: 'kryptokrauts.chain' });
-        window.superheroButton('.superhero-large', { size: 'large', account: 'kryptokrauts.chain' });
+        window.superheroUtils.createButton('.superhero-icon', { size: 'icon', account: 'kryptokrauts.chain' });
+        window.superheroUtils.createButton('.superhero-small', { size: 'small', account: 'kryptokrauts.chain' });
+        window.superheroUtils.createButton('.superhero-medium', { size: 'medium', account: 'kryptokrauts.chain' });
+        window.superheroUtils.createButton('.superhero-large', { size: 'large', account: 'kryptokrauts.chain' });
     }
     const handleChangeClientState = (newState, addedTags) => {
         if (addedTags && addedTags.scriptTags) {
@@ -16,7 +16,7 @@ export default () => {
     }
     return <>
         <Helmet
-            script={[{ src: 'https://unpkg.com/@aeternity/superhero-utils/dist/index.js' }]}
+            script={[{ src: 'https://unpkg.com/@aeternity/superhero-utils@0.5.1/dist/index.js' }]}
             onChangeClientState={handleChangeClientState}
         />
     </>
